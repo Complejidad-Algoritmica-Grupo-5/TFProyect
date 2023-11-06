a324b3a6dd1d6aea9a69b1ed10421561b77f4a79
AlbaCodePe <105762929+AlbaCodePe@users.noreply.github.com>
2023-11-06 14:39:47 -0500

    Update main.py
    added validations

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from graph_visualization import visualize_shortest_path
 
 def main():
-    start_node = int(input("Ingrese el nodo inicial: "))
-    end_node = int(input("Ingrese el nodo final: "))
-    visualize_shortest_path(start_node, end_node)
+    try:
+        start_node = int(input("Ingrese el nodo inicial: "))
+        end_node = int(input("Ingrese el nodo final: "))
+        visualize_shortest_path(start_node, end_node)
+    except ValueError:
+        print("Error: Ingrese números enteros para los nodos.")
+    except FileNotFoundError:
+        print("Error: No se encontraron archivos de datos en la ubicación especificada.")
 
 if __name__ == "__main__":
     main()
