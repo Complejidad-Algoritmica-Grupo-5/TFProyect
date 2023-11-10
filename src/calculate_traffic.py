19d7c3861f3a7a91f95a4eb43329af67e5f3b606
HectorQR <89368384+HectorQR@users.noreply.github.com>
2023-11-10 18:44:25 -0500

    Create calculate_traffic.py

---
+++ src/calculate_traffic.py
@@ -0,0 +1,13 @@
+def calculate_traffic(weight, hour):
+    # Calcula el factor de trafico
+    if hour < 7 or hour >= 20:
+        traffic_factor = 1.5
+    elif hour >= 7 and hour < 10:
+        traffic_factor = 1.2
+    elif hour >= 10 and hour < 17:
+        traffic_factor = 1.0
+    else:
+        traffic_factor = 0.8
+
+    # Actualiza el peso de la arista
+    return weight * traffic_factor
