27b96efaf74e629e299c8511b20327e83a034556
AlbaCodePe <105762929+AlbaCodePe@users.noreply.github.com>
2023-11-18 17:29:19 -0500

    Actualizar main.py
    new ui without red line

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
 from graph_visualization import visualize_shortest_path
-from data_processing import load_graph_data
 
 def is_valid_number(entry):
     try:
@@ -10,14 +9,13 @@ def is_valid_number(entry):
     except ValueError:
         return False
 
-def visualize_path(start_entry, end_entry, hour_entry, graph_data):
-    if hour_entry and is_valid_number(start_entry) and is_valid_number(end_entry) and is_valid_number(hour_entry):
+def visualize_path(start_entry, end_entry):
+    if is_valid_number(start_entry) and is_valid_number(end_entry):
         start_node = int(start_entry.get())
         end_node = int(end_entry.get())
-        hour = int(hour_entry.get())
-        visualize_shortest_path(start_node, end_node, graph_data, hour)
+        visualize_shortest_path(start_node, end_node)
     else:
-        messagebox.showerror("Error", "Por favor ingrese números válidos para todos los campos.")
+        messagebox.showerror("Error", "Por favor ingrese números válidos.")
 
 def center_window(window):
     window.update_idletasks()
@@ -35,7 +33,6 @@ def add_hour_entry(frame):
 
     hour_entry = tk.Entry(frame, font=("Arial", 12))
     hour_entry.grid(row=2, column=1, padx=5, pady=5)
-    return hour_entry
 
 def main():
     window = tk.Tk()
@@ -60,20 +57,14 @@ def main():
     end_entry = tk.Entry(frame, font=("Arial", 12))
     end_entry.grid(row=1, column=1, padx=5, pady=5)
 
-    hour_entry = None
-
     def visualize_and_center():
-        nonlocal hour_entry
-        hour_entry = add_hour_entry(frame)
+        visualize_path(start_entry, end_entry)
 
-    graph_data = load_graph_data()  # Asegúrate de proporcionar el graph_data aquí
-
-    visualize_button = tk.Button(window, text="Visualizar Ruta", command=lambda: visualize_path(start_entry, end_entry, hour_entry, graph_data), font=("Arial", 12))
+    visualize_button = tk.Button(window, text="Visualizar Ruta", command=visualize_and_center, font=("Arial", 12))
     visualize_button.pack(pady=10)
 
     def add_hour():
-        nonlocal hour_entry
-        hour_entry = add_hour_entry(frame)
+        add_hour_entry(frame)
 
     load_map_button = tk.Button(window, text="Incluir hora", command=add_hour, font=("Arial", 12))
     load_map_button.pack(pady=10)
@@ -83,4 +74,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
